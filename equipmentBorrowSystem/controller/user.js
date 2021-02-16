7c49d470340f4f973646ef7cd55191d7714d3afb
SoonWhy <763652253@qq.com>
2021-02-16 15:08:25 +0800

    修正了许多呜呜

--- equipmentBorrowSystem/controller/user.js
+++ equipmentBorrowSystem/controller/user.js
@@ -1,4 +1,4 @@
-const modules = require("../module/index");
+const models = require("../model/index");
 
 exports.putBorrowApply = async (body, params) => {
     let ret;
@@ -7,35 +7,45 @@ exports.putBorrowApply = async (body, params) => {
     let reason = body.reason;
     let contactInfo = body.contactInfo;
     let returnTime = body.returnTime;
-    let stuID = 3190105240;
+    let stuID = 3190105240; // 暂时写死
 
-    let moduleResult = await modules.module.userModule.putBorrowApply(
-        equipmentID,
-        startTime,
-        reason,
-        contactInfo,
-        returnTime,
-        stuID
-    );
+    let longestTime = await models.userModel.getLongestTime(equipmentID);
 
-    if (moduleResult == 200) {
-        ret = {
-            errorCode: 200,
-            errorMsg: "借用成功",
-            payload: {}
+    if (returnTime - startTime <= longestTime && returnTime > startTime) {
+        let moduleResult = await models.userModel.putBorrowApply(
+            equipmentID,
+            startTime,
+            reason,
+            contactInfo,
+            returnTime,
+            stuID
+        );
+
+        if (moduleResult) {
+            ret = {
+                errorCode: 200,
+                errorMsg: "借用成功",
+                payload: {},
+            };
+        } else {
+            ret = {
+                errorCode: 400,
+                errorMsg: "操作数据库出错",
+                payload: {},
+            };
         }
-    } else if (moduleResult == 400) {
-        ret = {
-        errorCode: 400,
-        errorMsg: "参数错误，借用失败",
-        payload: {}
-        }
-    } else if (!moduleResult) {
+    } else if (!longestTime) {
         ret = {
             errorCode: 400,
             errorMsg: "操作数据库出错",
-            payload: {}
-        }
+            payload: {},
+        };
+    } else {
+        ret = {
+            errorCode: 400,
+            errorMsg: "参数错误，借用失败",
+            payload: {},
+        };
     }
 
     return ret;
